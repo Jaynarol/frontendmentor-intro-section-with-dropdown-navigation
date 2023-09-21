460f00bd07a02a159ff8d6f16bbe58240cc0b5b0
Jaynarol <jaynarol@gmail.com>
2023-09-21 11:29:05 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+const Home = () => (
+  <main className="flex min-h-screen flex-col items-center justify-center bg-white text-lg dark:bg-slate-700">
+    Hello World!!
+  </main>
+)
+
+export default Home
